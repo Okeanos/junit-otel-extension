dc24edeefe401739b2c23446206967597d2bb5c3
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2024-05-06 17:55:46 +0200

    feat: add assertions to verify that metrics are correctly created

--- src/test/java/com/nikolasgrottendieck/junit/otel/metrics/BasicMetricsTest.java
+++ src/test/java/com/nikolasgrottendieck/junit/otel/metrics/BasicMetricsTest.java
@@ -2,20 +2,27 @@ package com.nikolasgrottendieck.junit.otel.metrics;
 
 import com.nikolasgrottendieck.helper.TestCase;
 import com.nikolasgrottendieck.junit.otel.OpenTelemetryMetrics;
+import io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions;
+import io.opentelemetry.sdk.testing.junit5.OpenTelemetryExtension;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import static com.nikolasgrottendieck.helper.TestCaseHelpers.engineTestKit;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 class BasicMetricsTest {
+
+	@RegisterExtension
+	private static final OpenTelemetryExtension otelTesting = OpenTelemetryExtension.create();
 
 	@Test
 	void verifyMetrics() {
@@ -29,6 +36,45 @@ class BasicMetricsTest {
 					.succeeded(1)
 					.aborted(1)
 					.failed(1)
+			);
+
+		assertThat(otelTesting.getMetrics())
+			.satisfiesExactlyInAnyOrder(
+				metricData ->
+					OpenTelemetryAssertions.assertThat(metricData)
+						.hasName("org.junit.test.aborted")
+						.hasLongSumSatisfying(
+							sum -> sum.hasPointsSatisfying(point -> point.hasValue(1)
+							)
+						),
+				metricData ->
+					OpenTelemetryAssertions.assertThat(metricData)
+						.hasName("org.junit.test.counter")
+						.hasLongSumSatisfying(
+							sum -> sum.hasPointsSatisfying(point -> point.hasValue(4)
+							)
+						),
+				metricData ->
+					OpenTelemetryAssertions.assertThat(metricData)
+						.hasName("org.junit.test.disabled")
+						.hasLongSumSatisfying(
+							sum -> sum.hasPointsSatisfying(point -> point.hasValue(1)
+							)
+						),
+				metricData ->
+					OpenTelemetryAssertions.assertThat(metricData)
+						.hasName("org.junit.test.failed")
+						.hasLongSumSatisfying(
+							sum -> sum.hasPointsSatisfying(point -> point.hasValue(1)
+							)
+						),
+				metricData ->
+					OpenTelemetryAssertions.assertThat(metricData)
+						.hasName("org.junit.test.successful")
+						.hasLongSumSatisfying(
+							sum -> sum.hasPointsSatisfying(point -> point.hasValue(1)
+							)
+						)
 			);
 	}
 
